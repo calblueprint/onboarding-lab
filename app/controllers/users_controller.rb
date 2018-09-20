95af222ca0c9708fb315040bb851d756b990d960
Ethan Lee <hello@ethanl.ee>
2018-09-19 19:02:46 -0700

    Update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def login
+  end
+
+  def signup
+  end
+
+  def create
+    # Your code here!
+  end
+
+  def friends
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: 'users#index'
+  get '/login', to: 'users#login'
+  get '/signup', to: 'users#signup'
+  post '/signup', to: 'users#create'
+  get '/friends', to: 'users#friends'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
